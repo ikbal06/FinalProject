d742535d1c876fd8bd1057cf802793ab8ba5a789
ikbal kırklar <ikbalkirklar@outlook.com>
2021-02-03 23:46:55 +0300

    3.02.21

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID o sunu yapıyoruz
+    //Open Closed Principle
+    //mevcut kodlara dokunmadan işini hallet
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll()) 
+            ProductManager productManager = new ProductManager(new EFProductDal());
+            foreach (var product in productManager.GetByUnitPrice(10,100)) 
             {
                 Console.WriteLine(product.ProductName);
             }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //sınıflandırmak istiyorum T yi
+    //generic constraint
+    //class demek referans tip demek
+    //herhangi bir class eklemesin mesela benim elimdekilerin özelliği entity de olmaları
+    //IEntity : IEntity olabilir ya da IEntity implemente edebilmeli
+    //new(): new lenebilir olmalı hani IEntity interface old new lenemiyor ya ondan faydalı burda new demek
+
+   public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        //tek bir data getirmek için tek bir kredinin gelmesi mesela birçok hesabın var onlardan birini seçmende kullanılır. null deemek filtresşşz yani hepsi geliecektir    
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
